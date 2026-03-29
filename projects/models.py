3bb93352a6a6f1d593b5152d708cfed8b17cbb4a
Kzaai <karol.zaiska@gmail.com>
2026-03-29 21:55:00 +0200

    Establish relationship between Projects and Tasks using ForeignKey

--- projects/models.py
+++ projects/models.py
@@ -1,13 +1,22 @@
 from django.db import models
+from projects.models import Project
 
-class Project(models.Model):
-    title = models.CharField(max_length=200) #tytul
-    description = models.TextField() #opis 
-    technology = models.CharField(max_length=100) #technologia
-    created_at = models.DateTimeField(auto_now_add=True) #data utworzenia
+class Task(models.Model):
 
+    project =  models.ForeignKey(Project, on_delete=models.CASCADE, related_name='tasks')
 
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+
+    STATUS_CHOICES = [
+        ('TODO', 'To Do'),
+        ('IN_PROGRESS', 'In Progress'),
+        ('DONE', 'Done'),
+    ]
+    status = models.CharField(max_lenght=20, choices=STATUS_CHOICES, default='TODO')
+
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return f"{self.project.title}] - {self.title}"
     
