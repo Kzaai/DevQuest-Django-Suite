ea1cff5f4b8d0d8cc03fd66675095f8a19bc6997
Kzaai <karol.zaiska@gmail.com>
2026-03-29 22:06:43 +0200

    Fix admin.E108 error: rename 'name' to 'title' in ProjectAdmin

--- projects/admin.py
+++ projects/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from .models import Project
 
-admin.site.register(Project)
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'technology', 'created_at')
+    search_fields = ('title', 'technology')
