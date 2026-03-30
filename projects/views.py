af713453b9df1066c0933085882882e38914676c
Kzaai <karol.zaiska@gmail.com>
2026-03-30 18:21:29 +0200

    Applied migrations for tasks app to fix OperationalError

--- projects/views.py
+++ projects/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 from .models import Project
+from tasks.models import Task
+
 
 def project_list(request):
     projects = Project.objects.all()
-    return render(request, 'projects/project_list.html', {'projects': projects})
+    tasks = Task.objects.all()
+
+    print(f"Znaleziono projektów : {projects.count()}")
+    
+
+    return render(request, 'projects/project_list.html', {'projects': projects, 'tasks': tasks})
