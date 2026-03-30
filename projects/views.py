d785bac618b83567f639ff54a244312def08699c
Kzaai <karol.zaiska@gmail.com>
2026-03-30 20:19:01 +0200

    Full navigation works: List to Detail view connection

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404 
 from .models import Project
 from tasks.models import Task
 
 
 def project_list(request):
+
     projects = Project.objects.all()
     tasks = Task.objects.all()
 
-    print(f"Znaleziono projektów : {projects.count()}")
-    
+    return render(request, 
+                  'projects/project_list.html', 
+                  {'projects': projects, 'tasks': tasks})
 
-    return render(request, 'projects/project_list.html', {'projects': projects, 'tasks': tasks})
+
+def project_detail(request, pk):
+
+    project = get_object_or_404(Project, pk=pk)
+    tasks = Task.objects.filter(project=project)
+
+    return render(request, 
+                  'projects/project_detail.html', 
+                  {'project': project, 'tasks': tasks})
